cc3aa597a7450dd99d9e74ce6db34ccd4bd05aa8
mahmudur1611 <78032950+mahmudur1611@users.noreply.github.com>
2021-05-18 10:12:12 -0400

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,127 @@
+// Start Preloader
+setTimeout(function(){
+  $('.loader-bg').fadeToggle();
+}, 2000);
+// End Preloader
+
+
+// Start Scroll Animation
+$(window).scroll(function(){
+    if($(window).scrollTop()){
+        $("nav").addClass("black");
+    }
+    else{
+        $("nav").removeClass("black");
+    }
+});
+// End Scroll Animation
+
+// Start Animate Typewritter
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+// End Animate Typewritter
+
+// Start Progress Bar
+$(document).ready(function(){
+    $(".progress-bar").ProgressBar();
+  });
+// End Progress Bar
+
+// Start Counter
+$('.counter').countUp();
+
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+// End Counter
+
+// Start MixItUp
+$(document).ready(function(){
+  var mixer = mixitup('.portfolio-items');
+})
+// End MixItUp
+
+// Start Review Stilder
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 1500,
+  autoplayHoverPause: true,
+  responsive: {
+    0:{
+      items:1,
+      nav: false
+    },
+    600:{
+      items:2,
+      nav: false
+    },
+    1000:{
+      items:3,
+      nav: false
+    }
+  }
+});
+// End Review Stilder
